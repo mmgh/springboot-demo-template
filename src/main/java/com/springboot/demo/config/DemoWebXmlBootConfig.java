10750ee97364d65b9ffab2d6e1796c7312cffb80
mmgh <forstill@gmail.com>
2020-09-01 22:34:23 +0900

    modified DemoWebXmlBootConfig xml setting

--- src/main/java/com/springboot/demo/config/DemoWebXmlBootConfig.java
+++ src/main/java/com/springboot/demo/config/DemoWebXmlBootConfig.java
@@ -5,16 +5,17 @@ import javax.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 
 @Configuration
-//@ImportResource("classpath:demo-config/*.xml")
+@ImportResource("classpath:demo-config/*.xml")
 public class DemoWebXmlBootConfig {
 
 	private static Logger logger = LoggerFactory.getLogger(DemoWebXmlBootConfig.class);
 	
 	@PostConstruct
 	public void init() {
-		logger.info("[fwk]DemoWebBootConfig init....");
+		logger.info("[fwk]DemoWebXmlBootConfig init....");
 	}
 	
 }
